004231d354622a3adef57d95333998639294b14d
SakaiKeita <32003@ojs.ac.jp>
2021-05-25 11:34:51 +0900

    P58リスト2,14 売り上げクラス作成

---
+++ SalesCalculator/Sale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    class Sale {
+   //売り上げクラス
+    public string Shopping { get; set; }//Shoppingを呼ぶとセッターゲッターのようになる。プライベートにもできる
+    //商品カテゴリー
+    public string ProductCatetgory { get; set; }   
+    //売上高
+    public int Amount { get; set; }
+        
+
+
+
+
+
+
+
+
+    }
+}
