7e70e36a076dc39b4e47d924ed3705b12e168ade
SakaiKeita <32003@ojs.ac.jp>
2021-05-25 10:26:27 +0900

    プロジェクト ファイルを追加します。

---
+++ DistanceConverter/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    /*FeetConberter*/
+    class Class1 {
+        public double FromMeter(double meter) {
+            return meter / 0.3048;
+        }
+        public double ToMeter(double feet) { 
+        return feet *0.3048;
+    }
+    
+    
+    }
+}
---
+++ DistanceConverter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    class Program {
+        static void Main(string[] args) {
+            if(args.Length >= 1 && args[0] == "-tom") {
+                // フィートからメートルへの対応表を出力
+                PrintFeetToMeterList(1, 10);
+            } else {
+                // メートルからフィートへの対応表を出力
+                PrintMeterToFeetList(1, 10);
+            }
+        }
+        // フィートからメートルへの対応表を出力 
+        private static void PrintFeetToMeterList(int start, int stop) {
+            Class1 converter = new Class1();
+
+            
+            
+            for(int feet = start; feet <= stop; feet++) {
+                double meter =converter.ToMeter(feet);
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+            }
+        }
+        // メートルからフィートへの対応表を出力
+        private static void PrintMeterToFeetList(int start, int stop) {
+            Class1 converter = new Class1();
+            for(int meter = start; meter <= stop; meter++) {
+                
+                double feet = converter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
+            }
+        }
+
+
+
+    }
+}
