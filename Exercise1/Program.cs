0004e00b81fa3922e98c0c2c8df0eec5d07256cf
SakaiKeita <32003@ojs.ac.jp>
2021-06-01 09:32:57 +0900

    演習2.1完成

---
+++ Exercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+            //2.1.3
+            var songs = new Song[] {
+                new Song("タイトル", "アーティスト名", 0),
+                new Song("Bride Over", "Simon&Garfunke", 323),
+                new Song("Close to you", "Capen", 545),
+                new Song("Honesty", "BillyJoel",222),
+                 new Song("I will Always", "Whitnhey Houston", 124)
+            };
+            PrintSongs(songs);
+        }
+        //  2.1.4
+        private static void PrintSongs(Song[] songs) {
+            foreach(var song in songs) {
+                Console.WriteLine(@"{0},{1},{2:m\:ss}", song.Title, song.ArtistName, song.Length);
+
+
+
+
+            }
+        }
+    }
+}
---
+++ Exercise1/Song.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+
+
+
+
+    
+    class Song {
+       public String Title { get; set; }
+       public String ArtistName { get; set; }
+       public int Length { get; set; }
+       
+        
+        //コンストラクタ
+        public Song(string title,String artistName,int length) {
+            this.Title = title;
+            this.ArtistName = artistName;
+            this.Length = length;
+        }
+    
+    
+    
+    
+    
+    
+    }
+}
