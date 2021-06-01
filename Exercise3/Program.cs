58047a0a337a17e16206ae5e0f71c820f6527f9f
SakaiKeita <32003@ojs.ac.jp>
2021-06-01 11:24:40 +0900

    P792.3完成

---
+++ Exercise3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    class Program {
+        static void Main(string[] args) {
+
+            var sales = new SalesCounter("Sales.csv");
+
+            //var amountPerStore = sales.GetPerStoreSales();//商品別カテゴリー別の売り上げを求める。
+            var amountPerCategory = sales.GetPerCategorySales();
+            foreach(var obj in amountPerCategory) {
+                Console.WriteLine("{0} {1:#,#}円", obj.Key, obj.Value);
+            }
+        }
+    }
+}
