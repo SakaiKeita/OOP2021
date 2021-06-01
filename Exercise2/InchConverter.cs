bab0a4d6b97c07ea3f6dc6254d0a8bb0f903adeb
SakaiKeita <32003@ojs.ac.jp>
2021-06-01 10:26:25 +0900

    79問題2,2完成

---
+++ Exercise2/InchConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    //フィートとメートルの単位変換クラス(静的クラス)
+
+    /*FeetConberter*///インチとメートルの単位変換クラス（静的）
+    public static class Class1 {
+        private const double ratio = 0.3048;
+
+
+
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+        public static double ToMeter(double feet) {
+            return feet * ratio;
+        }
+    }
+}
+
---
+++ Exercise2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            if(args.Length >= 1 && args[0] == "-tom") {
+                // フィートからメートルへの対応表を出力
+                PrintMeterToFeetList(1, 10);
+            } else {
+                // メートルからフィートへの対応表を出力
+                PrintInchToMeterList(1, 10);
+            }
+
+
+           
+
+        }
+
+        private static void PrintInchToMeterList(int start, int stop) {
+
+            for(int meter = start; meter <= stop; meter++) {
+
+
+                double inch = Class1.ToMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} inch", meter, inch);
+            }
+        }
+        private static void PrintMeterToFeetList(int start, int stop) {
+
+
+            for(int meter = start; meter <= stop; meter++) {
+
+                double inch = Class1.FromMeter(meter);
+                Console.WriteLine("{0} inch = {1:0.0000}m ", inch, meter);
+            }
+        }  
+        }
+}
