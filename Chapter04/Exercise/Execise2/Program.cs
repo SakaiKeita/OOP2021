2f68f11adda58dddf84f58e66b705d227f66c073
SakaiKeita <32003@ojs.ac.jp>
2021-06-11 15:52:14 +0900

    P133演習問題

---
+++ Chapter04/Exercise/Execise2/Program.cs
@@ -0,0 +1,70 @@
+﻿using Execise1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execise2 {
+    class Program {
+        static void Main(string[] args) {
+            //4.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980,1),
+                new YearMonth(1990,4),
+                new YearMonth(2000,7),
+                new YearMonth(2010,9),
+                new YearMonth(2020,12),
+
+                };
+            //4,2,2
+            Execise2_2(ymCollection);
+            Console.WriteLine("------");
+            //4,2,3
+
+            //4,2,4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("------");
+
+            Exercise2_5(ymCollection);
+
+
+
+        }
+        public static void Execise2_2(YearMonth[] ymCollection) {
+
+            foreach(var ym in ymCollection) {
+
+                Console.WriteLine(ym);
+            }
+
+        }
+
+
+
+        //4,2,3
+        static YearMonth FindFirst21C(YearMonth[] ymCollection) {
+
+            foreach(var ym in ymCollection) {
+                if(ym.Is21Century) 
+                  //  Console.WriteLine(ym);
+                    return ym;
+            }
+            return null;
+        }
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+          var yearmonth= FindFirst21C(ymCollection);
+            var s = yearmonth != null ? yearmonth.ToString():"21世紀のデータはありません";
+            Console.WriteLine(s);
+
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+         var array=ymCollection.Select(ym => ym.AddMonth()).ToArray();//ToArrayで配列にしている
+            foreach(var ym in array) {
+               
+                Console.WriteLine(ym);
+            }
+        }
+    }
+}
--- Chapter04/Section01/Program.cs
+++ Chapter04/Section01/Program.cs
@@ -7,6 +7,75 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
+            var Session = new Dictionary<string, object>();
+            Session["MyProduct"] = new Product();
+
+            var product = Session["MyProduct"] as Product;
+            if(product == null) {
+                //Productが取得できなかった時の処理
+                Console.WriteLine("productが取得できなかった");
+            } else {
+               //productが取得できたとき
+                Console.WriteLine("productが取得できた");
+            }
+        } 
+            //int count = 0;
+            //Console.WriteLine($"後置:{count++}");
+            //Console.WriteLine($"前置:{++count}");
+            // var ret = GetProductA();
+            //var list = new List<int> { 10, 20, 30, 40, };
+            //var key = 40;
+            //var num = list.Contains(key) ? 1 : 0;//成り立ったら1
+            //Console.Write(num);
+            //string code = "12345";
+            // var message = GetMessage(code) ?? DefaultMessage();
+            //Console.WriteLine(message);
         }
+
+      /*  private static Product GetProductA() {
+            Sale sale = new Sale();
+            sale.Product = new Product();
+            sale = null;
+            return sale?.Product;//null条件演算子
+        }
+
+*/
+
     }
-}
+     
+  /*  class Sale {
+        public string ShopName { get; set; } = "abcde";
+        public int Amount { get; set; } = 12340;
+        public Product Product { get; set; }
+    }
+    
+    
+  */  
+    
+    
+    
+    
+    
+    //スタブ
+       // private static object DefaultMessage() {  
+         //return "Default Message";   
+           // }
+
+    //private static object GetMessage(object code) {
+    //
+      //  return null;
+    
+    
+    //}
+
+
+            
+            
+            //0 var person = new Person {
+              //  Name = "あ",
+               // Birthday = new DateTime(1995, 11, 23),
+             //   PhoneNumber = "123-3456-789"
+           // };
+       // }
+           
+    
