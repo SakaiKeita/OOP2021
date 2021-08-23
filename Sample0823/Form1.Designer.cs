4a5f202bb046a3779a7ec1fd401fd2394c5b5932
敬太 酒井 <32003@ojs.ac.jp>
2021-08-23 16:12:01 +0900

    8月23日べき乗

---
+++ Sample0823/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Sample0823 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.Value = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Jyo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Exect = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Value
+            // 
+            this.Value.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Value.Location = new System.Drawing.Point(12, 12);
+            this.Value.Name = "Value";
+            this.Value.Size = new System.Drawing.Size(100, 37);
+            this.Value.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(139, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "の";
+            // 
+            // Jyo
+            // 
+            this.Jyo.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Jyo.Location = new System.Drawing.Point(195, 12);
+            this.Jyo.Name = "Jyo";
+            this.Jyo.Size = new System.Drawing.Size(100, 37);
+            this.Jyo.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(301, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "乗";
+            // 
+            // Exect
+            // 
+            this.Exect.Location = new System.Drawing.Point(395, 1);
+            this.Exect.Name = "Exect";
+            this.Exect.Size = new System.Drawing.Size(108, 53);
+            this.Exect.TabIndex = 2;
+            this.Exect.Text = "結果→";
+            this.Exect.UseVisualStyleBackColor = true;
+            this.Exect.Click += new System.EventHandler(this.Exect_Click);
+            // 
+            // Result
+            // 
+            this.Result.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Result.Location = new System.Drawing.Point(542, 17);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(100, 37);
+            this.Result.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Exect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Jyo);
+            this.Controls.Add(this.Value);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Value;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Jyo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Exect;
+        private System.Windows.Forms.TextBox Result;
+    }
+}
+
---
+++ Sample0823/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0823 {
+    public partial class Form1 : Form {
+        public Form1() {
+            int n;
+            InitializeComponent();
+        }
+
+        private void Exect_Click(object sender, EventArgs e) {
+            int.Parse( Value.Text);
+            int.Parse(Jyo.Text);
+            int.Parse(Result.Text);
+            
+            for(int i= 0;i<=Jyo; i++) {
+            
+                
+                Result = Value *i;
+            
+            }
+        
+        
+        }
+    }
+}
