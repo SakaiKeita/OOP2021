fe1ace3255fb21b9182aa840fef75879e62ee193
敬太 酒井 <32003@ojs.ac.jp>
2021-08-30 16:14:42 +0900

    0820

--- Chapter11/Exerces/Program.cs
+++ Chapter11/Exerces/Program.cs
@@ -16,8 +16,11 @@ namespace Exerces {
             Exercise1_2(file);
             Console.WriteLine("--------");
 
+            Exercise1_3(file);
+            Console.WriteLine("--------");
 
-
+            Exercise1_4(file);
+            Console.WriteLine("--------");
 
 
         }
@@ -36,35 +39,49 @@ namespace Exerces {
             }
 
         }
-    
 
 
 
 
-     private static void Exercise1_2(string file) {
-         var xdoc = XDocument.Load(file);
+
+        private static void Exercise1_2(string file) {
+            var xdoc = XDocument.Load(file);
             var sports = xdoc.Root.Elements().Select(x => new {
                 Firstplayed = x.Element("firstplayed").Value,
                 Name = x.Element("name").Attribute("kanji").Value
             }).OrderBy(x => int.Parse(x.Firstplayed));
 
             foreach(var sport in sports) {
-                Console.WriteLine("{0} {1}", sport.Name);
+                Console.WriteLine("{0} {1}", sport.Name, sport.Firstplayed);
 
             }
-
-
-
-        
-        
         }
+            private static void Exercise1_3(string file) {
+            var xdoc = XDocument.Load(file);
+            var sports = xdoc.Root.Elements().Select(x => new {
+                Name = x.Element("name").Value,
+                Teammembers = x.Element("teammembers").Value
+
+            })
+             .OrderByDescending(x => int.Parse(x.Teammembers)).First();
+            Console.WriteLine("{0}", sports.Name);
+            }
+
+        private static void Exercise1_4(string file) {
+            //var xdoc = XDocument.Load("Sample.xml");
+            var newfile = "sports.xml";
+            
+            
+           
+            var element = new XElement("Sample.xml", new XElement("name", "サッカー", new XElement("kanji", "蹴玉")),
+            new XElement("teammembers", "11"), new XElement("firstplayed", "1948"));
 
 
+            var xdoc = XDocument.Load("Sample.xml");
 
+            xdoc.Save("sports.xml", SaveOptions.DisableFormatting);
 
-
-     
-
-
+            xdoc.Root.Add(element);
+        }
 }
 }
