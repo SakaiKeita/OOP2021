af70b0db65d5323e8c826d3b5237f78ead0c91b5
敬太 酒井 <32003@ojs.ac.jp>
2021-11-12 15:56:57 +0900

    WPF問題

---
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumberGame {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+
+
+            Random rnd = new Random();
+
+            for(int j = 0; j < 10; j++) {
+                Console.Write("{0} ", rnd.Next(1, 25));
+            }
+            Console.WriteLine(Environment.TickCount);
+
+
+
+
+
+
+
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e) {
+            Random rnd = new Random();
+            if(rnd.Next(1, 25) == 1) {
+                Button1.
+            }
+
+
+        }
+    }
+}
+
