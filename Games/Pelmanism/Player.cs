b7d950cfd49139a8649da8a6df76821b45884643
敬太 酒井 <32003@ojs.ac.jp>
2021-11-18 11:44:02 +0900

    神経衰弱画面

---
+++ Games/Pelmanism/Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pelmanism {
+    class Player {
+        private int nowOpenCardIndex1;//一枚目に開いたカードの添え字
+        private int nowOpenCardIndex2;//２枚目に開いたカードの添え字
+        //前回一枚目に開いたカードの添え字
+        public int BeforeOpenCardIndex1 { get; set; }
+        //前回2枚目に開いたカードの添え字
+        public int BeforeOpenCardIndex2 { get; set; }
+        //めくった枚数
+        public int OpenCounter { get; set; }
+
+        //コンストラクター
+        public Player(){
+            nowOpenCardIndex1 = nowOpenCardIndex2 = -1;
+            BeforeOpenCardIndex2 = BeforeOpenCardIndex1 = -1;
+            OpenCounter = 0;
+
+}
+       //今回一枚目に開いたカードの添え字
+       public int NowOpenCardIndex1{
+
+            get{ return nowOpenCardIndex1; }
+            set{
+                nowOpenCardIndex1 = value;
+                OpenCounter++;
+            }
+
+        }
+        //今回2枚目に開いたカードの添え字
+        public int NowOpenCardIndex2{
+
+            get { return nowOpenCardIndex2; }
+            set {
+                nowOpenCardIndex2 = value;
+                OpenCounter++;
+            }
+
+        }
+
+        //カードの情報をリセット
+        public void Reset() {
+            NowOpenCardIndex1 = NowOpenCardIndex1;
+            BeforeOpenCardIndex2 = NowOpenCardIndex2;
+            NowOpenCardIndex1 = -1;
+            nowOpenCardIndex2 = -1;
+            OpenCounter = 0;
+
+        }
+
+
+
+    }
+}
--- WPF/NumberGame/MainWindow.xaml.cs
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,35 +23,29 @@ namespace NumberGame {
     public partial class MainWindow : Window {
                  Random rnd = new Random();
         int r1 = 0;
-
+        int stop =0;
+        int x;
         public MainWindow() {
-
-
-
             r1 = rnd.Next(1, 26);
 
-
-
-
-
+           
 
 
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
+          
+
             Button btn = (Button)sender;
-            if(int.Parse(btn.Content.ToString()) == r1) {
-                Ransuu.Text = "正解";
-            }
-            else if(int.Parse(btn.Content.ToString()) <r1){
-
-                Ransuu.Text = "小さい";
-            }
-            else { Ransuu.Text = "大きい"; }
+                if(int.Parse(btn.Content.ToString()) == r1) {
+                    Ransuu.Text = "正解"
+                    
+                
+                ;
+                } else if(int.Parse(btn.Content.ToString()) < r1) {
 
 
-
-
+                } else { Ransuu.Text = "大きい"; }
 
         }
 
